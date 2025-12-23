bfefa06b1868d775677262955f00bcaf1c85969e
Young tech <younghustler252@email.com>
2025-12-23 18:48:52 +0100

    fix spelling mismatch

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SideBar";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
